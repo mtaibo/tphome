6da59941ceb5ca9fe83de3850c2a4a49fce0f8f8
Miguel Taibo <dev@migueltaibo.com>
2026-01-24 19:06:41 +0100

    Added first not working version of an example of top button

---
+++ firmware/src/config.cpp
@@ -0,0 +1,10 @@
+#include <config.h>
+
+Config config;
+
+void config_init() {
+
+    config.short_pulse = 3000;
+    config.long_pulse = 10000;
+
+}
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,9 +1,65 @@
 #include <Arduino.h>
 
-void setup() {
+#include "config.h"
+#include "pins.h"
 
+int input_pins[] = {BTN_TOP, BTN_BOTTOM};
+int output_pins[] = {LED_TOP, LED_MID, LED_BOTTOM, LED_GREEN, RELAY_UP, RELAY_DOWN};
+
+int button_pressed = -1;
+unsigned long time_pressed = 0;
+
+void setup() {
+    Serial.begin(115200);
+
+    // Loop to turn every output pin to output mode
+    for (int i=0; i<6; i++) {
+        pinMode(output_pins[i], OUTPUT);
+    }
+    // Loop to turn every input pin to input_pullup mode
+    for (int i=0; i<2; i++) {
+        pinMode(input_pins[i], INPUT_PULLUP);
+    }
 }
 
 void loop() {
 
+    if (digitalRead(BTN_TOP) == LOW && button_pressed == -1) {
+
+        button_pressed = BTN_TOP;
+        time_pressed = millis();
+        unsigned long duration = 0;
+
+        while (digitalRead(BTN_TOP) == LOW) {
+            duration = millis() - time_pressed;
+            delay(10); 
+        }
+
+        Serial.print("Boton soltado. Duracion: ");
+        Serial.println(duration);
+        Serial.print(config.short_pulse, config.long_pulse);
+
+        if (duration < config.short_pulse) {
+            Serial.println("ACCION CORTA");
+            digitalWrite(LED_TOP, HIGH);
+            digitalWrite(RELAY_UP, HIGH);
+            delay(3000);
+            digitalWrite(LED_TOP, LOW);
+            digitalWrite(RELAY_UP, LOW);
+        } else if (duration < config.long_pulse) {
+            Serial.println("ACCION MEDIA");
+            for (int i=0; i<10; i++) {
+                digitalWrite(LED_TOP, HIGH); delay(200);
+                digitalWrite(LED_TOP, LOW);  delay(200);
+            }
+        } else {
+            Serial.println("ACCION LARGA");
+            for (int i=0; i<10; i++) {
+                digitalWrite(LED_GREEN, HIGH); delay(200);
+                digitalWrite(LED_GREEN, LOW);  delay(200);
+            }
+        }
+
+        button_pressed = -1; 
+    }
 }
