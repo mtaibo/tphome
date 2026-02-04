e0a7326fc0d03eab14b9bebe5e1eabc9a31e65b8
Miguel Taibo <dev@migueltaibo.com>
2026-02-04 12:24:39 +0100

    Added a better topic managment, comments and state messages when goind up, down or stop

--- firmware/src/network.cpp
+++ firmware/src/network.cpp
@@ -10,29 +10,47 @@ PubSubClient client(espClient);
 
 void callback(char* topic, byte* payload, unsigned int length) {
 
+    // Common messages received on the chip to set main actions
     if (!strcmp(topic, config.set_topic)) { 
 
-        if (length == 2 && !memcmp(payload, "UP", 2))    handle_button_action(BTN_TOP, 200); 
+        // Orders to go UP, DOWN or to STOP
+        if (length == 2 && !memcmp(payload, "UP", 2)) handle_button_action(BTN_TOP, 200); 
         else if(length == 4) {
             if (!memcmp(payload, "STOP", 4)) handle_button_action(BTN_MID, 200);
             else if (!memcmp(payload, "DOWN", 4)) handle_button_action(BTN_BOTTOM, 200);
         }
+
+        // Order to set the position of the blind to a determined pos
+        if (length >= 4 && !memcmp(payload, "SET", 3)) {
+            // TODO: When blind position gets implemented, this will be next
+        }
     } 
 
-    else if (!strcmp(topic, config.admin_topic)) {
-        return;
+    // Messages received on the chip from admin to change configuration
+    // or to publish configuration elements from the chip
+    else if (!strcmp(topic, config.admin_set_topic)) {
+        
+        // TODO: Possible commands on admin topic are pending to be created
+
+        if (length == 6 && !memcmp(payload, "GET_ID", 6)) {
+            client.publish(config.admin_state_topic, config.device_id);
+        }
+
+        else if (length == 10 && !memcmp(payload, "GET_CONFIG", 10)) {
+            client.publish(config.admin_state_topic, "DICT");
+        }
     }
 }
 
 void topic_setup() {
 
-    // tphome/blinds/lounge/big-blind/set
-    // tphome/blinds/lounge/big-blind/state
-    // tphome/admin/DEVICE_ID
-
+    // This lines create the topics depending on the device room, name and device_id
     snprintf(config.set_topic, sizeof(config.set_topic), "tphome/blinds/%s/%s/set", config.room, config.name);
     snprintf(config.state_topic, sizeof(config.state_topic), "tphome/blinds/%s/%s/state", config.room, config.name);
-    snprintf(config.admin_topic, sizeof(config.admin_topic), "tphome/admin/%s", config.device_id);
+    snprintf(config.admin_set_topic, sizeof(config.admin_set_topic), "tphome/admin/%s/set", config.device_id);
+    snprintf(config.admin_state_topic, sizeof(config.admin_state_topic), "tphome/admin/%s/state", config.device_id);
+
+    client.disconnect();
 }
 
 void network_setup() {
@@ -42,23 +60,27 @@ void network_setup() {
     WiFi.begin(config.wifi_ssid, config.wifi_password);
     
     // MQTT server configuration
-    client.setServer(config.mqtt_server, 1883);
+    client.setServer(config.mqtt_server, config.mqtt_server_port);
     client.setCallback(callback);
 
     // Topic setup
     topic_setup();
 }
 
-// Function to connect to the MQTT server with its credentials
-// and also to subscribe to the corresponding topic
 bool mqtt_reconnect() {
 
+    // Connect to the MQTT server with its credentials
     if (client.connect(config.device_id, config.mqtt_user, config.mqtt_pass)) {
         
+        // When connected, this two lines below subscribe the client 
+        // to the set topics to receive changes from the broker
         client.subscribe(config.set_topic); 
-        client.subscribe(config.admin_topic);
+        client.subscribe(config.admin_set_topic);
+
+        // Message sent when connection was succesful
         client.publish(config.state_topic, "CONNECTED", true);
         
+        // Stop led blinking and return true as the connection was succesful
         blink(LED_GREEN, 0); return true;
 
     } return false;
@@ -67,19 +89,19 @@ bool mqtt_reconnect() {
 void network_check() {
 
     // This line activates the green_led while the wifi is not connected
-    if (WiFi.status() != WL_CONNECTED) {if (!config.is_blinking) { blink(LED_GREEN, 1); config.blink_time = 500; } return; } 
+    if (WiFi.status() != WL_CONNECTED) {if (!config.is_blinking) { blink(LED_GREEN, 1); config.blink_time = 10000; } return; } 
 
     // Gestión de MQTT
     if (!client.connected()) {
 
         // Section to control blinking led when mqtt server disconnects
         if (!config.is_blinking) blink(LED_GREEN, 1);
-        config.blink_time = 1000;
+        config.blink_time = 500;
 
-        // This lines below manage a 5 second delay 
+        // This lines below manage a 4 second delay 
         // to reattempt the connection to the MQTT server
         static unsigned long last_reconnect_attempt = 0;
-        if (millis() - last_reconnect_attempt > 5000) { 
+        if (millis() - last_reconnect_attempt > 4000) { 
             last_reconnect_attempt = millis();
 
             // This line calls the mqtt_reconnect function 
