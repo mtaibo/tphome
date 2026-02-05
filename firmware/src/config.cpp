4f2885477770794de8367ad41af07641841e0c15
Miguel Taibo <dev@migueltaibo.com>
2026-02-05 12:15:50 +0100

    Added the manage_json function, not finished yet, but able to read json file. Also some code refactor con config files

--- firmware/src/config.cpp
+++ firmware/src/config.cpp
@@ -1,22 +1,44 @@
+#include <LittleFS.h>
+#include <ArduinoJson.h>
+
 #include <credentials.h>
 #include <config.h>
 #include <pins.h>
 
 Config config;
 
-void config_setup() {
+void pin_setup() {
 
-    // This both lines below activate the pin to allow
+   // This both lines below activate the pin to allow
     // the blue leds to turn on, otherwise, they'll be blocked
     pinMode(CONFIG_LED, OUTPUT);
     digitalWrite(CONFIG_LED, HIGH);
 
-    // Loop to turn every output pin to output mode
+    // Loop to turn every output pin to output mode and low
     int output_pins[] = {LED_TOP, LED_MID, LED_BOTTOM, LED_GREEN, RELAY_UP, RELAY_DOWN};
     for (int i=0; i<6; i++) {
         pinMode(output_pins[i], OUTPUT);
         digitalWrite(output_pins[i], LOW);
     }
+}
+
+bool manage_json(int mode) {
+    
+    // Get the config file from the flash memory
+    File configFile = LittleFS.open("/config.json", "r");
+    if (!configFile) return false;
+
+    // Get the json on "doc" from the file that was opened
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, configFile);
+
+    configFile.close(); // Close the file that was opened by LittleFS to free memory
+    if (error) return false; // If json is corrupted, return false after closing the configFile
+
+    return true;
+}
+
+void config_default() {
 
     // Device ID
     strcpy(config.device_id, DEVICE_ID);
@@ -31,11 +53,6 @@ void config_setup() {
     strcpy(config.mqtt_user, MQTT_USER);
     strcpy(config.mqtt_pass, MQTT_PASS);
 
-    config_default();
-}
-
-
-void config_default() {
 
     // ! VARIABLES THAT CAN BE MODIFIED
 
@@ -45,11 +62,11 @@ void config_default() {
     strcpy(config.name, "big-blind");
 
     // Blind configuration
-    config.up_time = 40000;
-    config.down_time = 40000;
+    config.up_time = DEFAULT_UP_TIME;
+    config.down_time = DEFAULT_DOWN_TIME;
     config.current_position = 50.0;
 
-    // ! VARIABLES THAT ARE JUST DEFAULT SETTINGS
+    // ! VARIABLES THAT ARE JUST EXECUTION SETTINGS
 
     // Execution data
     config.is_moving = false;
@@ -57,7 +74,6 @@ void config_default() {
 
     // Blinking config
     config.blink_time = 500;
-    config.blinking_led = -1;
     config.is_blinking = false;
     config.last_blink = 0;
 
@@ -67,4 +83,9 @@ void config_default() {
 
     config.short_pulse = 3000;
     config.long_pulse = 10000;
+}
+
+void config_setup() {
+    pin_setup();
+    if (!manage_json(0)) config_default();
 }
