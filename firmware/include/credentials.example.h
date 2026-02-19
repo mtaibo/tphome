33d70c2d714669dd3bb1f831e4bd7aaa821fd969
Miguel Taibo <dev@migueltaibo.com>
2026-02-19 12:06:53 +0100

    Added a credentials.example.h to the easy setup when downloading git repo

---
+++ firmware/include/credentials.example.h
@@ -0,0 +1,14 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#define DEVICE_ID "B364859"
+
+#define WIFI_SSID ""
+#define WIFI_PASS """
+
+#define MQTT_SERVER ""
+#define MQTT_PORT 1883
+#define MQTT_USER ""
+#define MQTT_PASS ""
+
+#endif
