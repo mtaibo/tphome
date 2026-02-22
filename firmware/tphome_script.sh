1621e69cb4cc9a99716ba03c7e5b54f6babd613a
mtaibo <dev@migueltaibo.com>
2026-02-22 10:40:50 +0000

    Corrected tphome_script.sh

--- firmware/tphome_script.sh
+++ firmware/tphome_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# --- VARIABLES CONFIGURABLES ---
 HOST="localhost"
 DEVICE_ID="B364859"
 LOCATION="secondary_bedroom"
@@ -10,32 +9,31 @@ DEVICE_TYPE="blind"
 ADMIN_BASE="tphome/admin"
 BLIND_BASE="tphome/blinds/$LOCATION/$DEVICE_TYPE"
 
-# --- LÓGICA DEL SCRIPT ---
 case "$1" in
 --admin)
   if [ "$2" == "--sub" ]; then
-    echo "Escuchando ADMIN: $ADMIN_BASE/+/state"
+    echo "Listening ADMIN: $ADMIN_BASE/+/state"
     mosquitto_sub -h "$HOST" -t "$ADMIN_BASE/+/state" -v
   elif [ "$2" == "--pub" ]; then
     MSG="$3"
-    echo "Publicando en ADMIN: $MSG"
+    echo "Publishing on ADMIN: $MSG"
     mosquitto_pub -h "$HOST" -t "$ADMIN_BASE/$DEVICE_ID/set" -m "$MSG"
   fi
   ;;
 
 --blind)
   if [ "$2" == "--sub" ]; then
-    echo "Escuchando PERSIANA: $BLIND_BASE/state"
+    echo "Listening BLIND: $BLIND_BASE/state"
     mosquitto_sub -h "$HOST" -t "$BLIND_BASE/state" -v
   elif [ "$2" == "--pub" ]; then
     MSG="$3"
-    echo "Publicando en PERSIANA: $MSG"
+    echo "Publish on BLIND: $MSG"
     mosquitto_pub -h "$HOST" -t "$BLIND_BASE/set" -m "$MSG"
   fi
   ;;
 
 *)
-  echo "Uso correcto:"
+  echo "Proper use:"
   echo "  tphome --admin [--sub | --pub 'MENSAJE']"
   echo "  tphome --blind [--sub | --pub 'MENSAJE']"
   ;;
