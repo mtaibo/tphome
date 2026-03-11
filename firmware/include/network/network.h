6e062a0d30577abc58a8586aa1f2cf2a8cda9146
mtaibo <dev@migueltaibo.com>
2026-03-11 07:06:49 +0000

    Added mqtt setup and update to network module

--- firmware/include/network/network.h
+++ firmware/include/network/network.h
@@ -3,15 +3,18 @@
 
 #include "settings.h"
 #include "wifi.h"
+#include "mqtt.h"
 
 namespace Network {
 
     void inline setup() {
         Wifi::setup();
+        Mqtt::setup();
     }
 
     void inline update() {
         Wifi::update();
+        Mqtt::update();
     }
 }
 
