dde30ad3f75592f6d4d47237363f507b42036fda
mtaibo <dev@migueltaibo.com>
2026-04-19 13:26:30 +0000

    feat: code refactor on app and sidebar, also addded getButtonClass function to have cleaner code

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <script setup>
-import { ref } from 'vue';
-import Sidebar from './components/layout/Sidebar.vue';
-import Topbar from './components/layout/Topbar.vue';
-import Dashboard from './views/Dashboard.vue';
 
-// Estado global de conexión (que luego actualizarás con WebSockets)
-const isOnline = ref(true);
+    import { ref } from 'vue';
+
+    import Sidebar from './components/layout/Sidebar.vue';
+    import Topbar from './components/layout/Topbar.vue';
+    import Dashboard from './views/Dashboard.vue';
+
+    const isOnline = ref(true); // API connection status
+
 </script>
 
 <template>
-  <div class="flex h-screen w-screen bg-tp-bg text-slate-200 overflow-hidden font-sans">
+
+  <div class="flex h-screen w-screen overflow-hidden font-sans bg-tp-bg text-slate-200">
     
     <Sidebar />
 
     <main class="flex-1 flex flex-col relative overflow-hidden">
+
       <Topbar :isOnline="isOnline" />
 
-      <section class="flex-1 relative overflow-hidden bg-[radial-gradient(var(--color-tp-border)_1px,transparent_1px)] [background-size:32px_32px]">
+      <section class="flex-1 relative overflow-hidden bg-grid-dots">
         <Dashboard />
       </section>
+
     </main>
 
   </div>
+
 </template>
