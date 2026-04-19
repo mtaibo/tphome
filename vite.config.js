a89e330f5c84b7780c36c8a681416e44be47d163
Miguel Taibo <dev@migueltaibo.com>
2026-04-19 10:49:38 +0200

    fix: modify tailwind css implementation to suit v4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite' // <--- 1. Importar
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss(), // <--- 2. Añadir a la lista de plugins
+  ],
 })
