16470d8e79e0383994c107fa4c4f4a36b5e7cc37
mtaibo <dev@migueltaibo.com>
2026-04-21 13:11:33 +0000

    docs: change comments on vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite' // <--- 1. Importar
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(), // <--- 2. Añadir a la lista de plugins
-  ],
+    tailwindcss(),
+  ]
 })
