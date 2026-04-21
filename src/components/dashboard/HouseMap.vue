be62fd3574b2f75081956733bd51300b2d97060d
Miguel Taibo <dev@migueltaibo.com>
2026-04-21 17:33:46 +0200

    feat: styled blinds appearance on house map

--- src/components/dashboard/HouseMap.vue
+++ src/components/dashboard/HouseMap.vue
@@ -109,26 +109,43 @@
             
             <!-- Blinds -->
             <g v-for="(blind, id) in blinds" :key="id" class="select-none cursor-pointer">
-                
+  
                 <rect 
-                    :x="blind.x" 
-                    :y="blind.y" 
-                    :width="blind.width" 
-                    :height="blind.height" 
+                    :x="blind.x" :y="blind.y" 
+                    :width="blind.width" :height="blind.height" 
                     rx="1.5"
-                    class="fill-tp-surface/10 stroke-tp-border stroke-[1px]"
+                    class="fill-black stroke-tp-border stroke-[1px]"
                 />
 
                 <rect 
-                    :x="blind.x" 
-                    :y="blind.y" 
+                    :x="blind.x" :y="blind.y" 
                     :width="blind.width > blind.height ? (blind.state.position / 100) * blind.width : blind.width" 
                     :height="blind.height > blind.width ? (blind.state.position / 100) * blind.height : blind.height" 
                     rx="1"
-                    class="fill-muted/40 transition-all duration-500 ease-in-out"
+                    class="fill-muted transition-all duration-500 ease-in-out"
+                />
+
+                <rect 
+                    :x="blind.x" :y="blind.y" 
+                    :width="blind.width > blind.height ? (blind.state.position / 100) * blind.width : blind.width" 
+                    :height="blind.height > blind.width ? (blind.state.position / 100) * blind.height : blind.height" 
+                    :fill="blind.width > blind.height ? 'url(#pattern-v)' : 'url(#pattern-h)'"
+                    class="pointer-events-none transition-all duration-500 ease-in-out"
                 />
 
             </g>
+
+
+            <!-- Patterns -->
+            <defs>
+                <pattern id="pattern-h" x="0" y="0" width="100%" height="4" patternUnits="userSpaceOnUse">
+                    <line x1="0" y1="0" x2="100%" y2="0" class="stroke-black/30" stroke-width="1"/>
+                </pattern>
+
+                <pattern id="pattern-v" x="0" y="0" width="4" height="100%" patternUnits="userSpaceOnUse">
+                    <line x1="0" y1="0" x2="0" y2="100%" class="stroke-black/30" stroke-width="1"/>
+                </pattern>
+            </defs>
 
         </svg>
     </div>
