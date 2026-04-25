2e74e53649d6b5e12297269660803cc927817de9
Miguel Taibo <dev@migueltaibo.com>
2026-04-25 08:45:54 +0200

    feat: add websockets router  and fixed connection manager class.

--- api/app/connections.py
+++ api/app/connections.py
@@ -1,5 +1,8 @@
-from fastapi.websockets import WebSocket
+from fastapi.websockets import WebSocket, WebSocketDisconnect
+from fastapi import APIRouter 
 from typing import List
+
+router = APIRouter(tags=["WebSocket"])
 
 class ConnectionManager:
 
@@ -11,10 +14,30 @@ class ConnectionManager:
         self.active.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
-        self.active.remove(websocket)
+        if websocket in self.active:
+            self.active.remove(websocket)
 
     async def broadcast(self, message: dict):
-        for connection in self.active:
-            await connection.send_json(message)
+        for connection in self.active[:]:
+            try: await connection.send_json(message)
+            except Exception: self.disconnect(connection)
 
 manager = ConnectionManager()
+
+
+@router.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+
+    await manager.connect(websocket)
+    
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast({
+                "client": client_id,
+                "data": data
+            })
+            
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast({"info": f"Client #{client_id} disconnected"})
