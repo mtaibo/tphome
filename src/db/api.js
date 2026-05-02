29c6891f5ec88733072488cb90f5d6e88db310a1
mtaibo <dev@migueltaibo.com>
2026-05-02 21:14:44 +0000

    feat: code refactor and changed some implementations, devices.js has much work to do.

--- src/db/api.js
+++ src/db/api.js
@@ -9,10 +9,23 @@ const client = axios.create({
 
 export const api = {
 
-    /* Function to get devices from api db */
-    async getDevices() {
-        const response = await client.get('/devices')
+    /* Function to get devices state from api db */
+    async getState() {
+        const response = await client.get('/devices/state')
         return response.data
+    },
+
+    /* Function to get the devices or the map config json file */
+    async getConfig(subject) {
+        let path = `/config/${subject}`
+        const response = await client.get(path)
+        return response.data
+    },
+
+    /* Function to publish the devices or the map new config json file */
+    async postConfig(subject, newConfig) {
+        let path = `/config/${subject}`
+        return client.post(path, newConfig)
     },
 
     /* Function to build commands requests */
@@ -22,16 +35,5 @@ export const api = {
         if (command === 'set' && value !== null) path = `/commands/${id}/set/${value}`
 
         return client.post(path)
-    },
-
-    /* Function to get the devices config json file */
-    async getConfig() {
-        const response = await client.get('/config/devices')
-        return response.data
-    },
-
-    /* Function to publish the devices config json file */
-    async postConfig(json) {
-        return client.post('/config/devices', json)
     },
 }
