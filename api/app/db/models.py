7b80752d8ef77560a4dd3a629fd34f0c81bdeda0
mtaibo <dev@migueltaibo.com>
2026-05-03 09:07:02 +0000

    feat: adjust db models to fit a more functional json for the frontend.

--- api/app/db/models.py
+++ api/app/db/models.py
@@ -2,21 +2,45 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
 
-class Zone(SQLModel, table=True):
-    code: str = Field(primary_key=True)
-    name: str
 
+'''
+{
+    "id": "B0101",
+
+    "hardware": {
+        "mac": "20:40",
+        "firmware_version": "3f.68.af"
+    },
+
+    "connection": {
+        "online": true,
+        "last_seen": "2026-04-29T16:40:11.675639Z"
+    },
+
+    "prefs": {
+        "up_time": 5242,
+        "down_time": 42926,
+        "down_pos": 61008,
+        "inverted_relays": true
+    },
+
+    "state": {
+        "position": 100,
+        "motor_state": 0
+    }
+}
+'''
 
 class Device(SQLModel, table=True):
     id: str = Field(primary_key=True)
-    mac: str
-    name: str
-    type: str
-    zone: str = Field(foreign_key="zone.code")
 
+    # Hardware
+    mac: str
+    firmware_version: Optional[str] = None
+
+    # Connection
     online: bool = False
     last_seen: Optional[datetime] = None
-    firmware_version: Optional[str] = None
 
 
 class PendingDevice(SQLModel, table=True):
@@ -26,15 +50,15 @@ class PendingDevice(SQLModel, table=True):
 class Blind(SQLModel, table=True):
     id: str = Field(foreign_key="device.id", primary_key=True)
 
-    # State
-    position: int = 0       # 0-100
-    motor_state: int = 0    # 0=IDLE 1=WAITING 2=MOVING 3=STOPPING
-
     # Prefs
     up_time: int = Field(default=0)
     down_time: int = Field(default=0)
     down_pos: int = Field(default=0)
     inverted_relays: bool = Field(default=False)
+
+    # State
+    position: int = 0
+    motor_state: int = 0
 
 
 class Light(SQLModel, table=True):
