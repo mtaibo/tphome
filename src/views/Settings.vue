aad2fd1ad4cdde014801c69847872c286ac010b5
Miguel Taibo <dev@migueltaibo.com>
2026-05-11 14:11:03 +0200

    feat: add a settings default view with just a back button, add settings routing to a button on sidebar.

---
+++ src/views/Settings.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+    import Sidebar from '../layout/Sidebar.vue';
+    import Topbar from '../layout/Topbar.vue';
+    import Blueprint from '../components/Blueprint.vue';
+
+</script>
+
+<template>
+
+  <div class="flex h-screen w-screen overflow-hidden font-sans bg-grid-dots text-slate-200 items-center justify-center">
+    
+    <RouterLink
+        to="/"
+        class="flex items-center gap-2 px-3 py-2 rounded-lg text-lg text-muted hover:text-white transition-colors duration-200"
+    >
+        <ArrowLeft class="w-4 h-4" />
+        <span>Volver</span>
+    </RouterLink>
+
+  </div>
+
+</template>
