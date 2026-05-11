9bd81fc62972b758966c5cb7a3d72f50feeec74a
Miguel Taibo <dev@migueltaibo.com>
2026-05-11 12:46:32 +0200

    feat: move default view from app to dashboard to allow a new settings view.

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+    import Sidebar from '../layout/Sidebar.vue';
+    import Topbar from '../layout/Topbar.vue';
+    import Blueprint from '../components/Blueprint.vue';
+
+</script>
+
+<template>
+
+  <div class="flex h-screen w-screen overflow-hidden font-sans bg-tp-bg text-slate-200">
+    
+    <Sidebar />
+
+    <main class="flex-1 flex flex-col relative overflow-hidden">
+
+      <Topbar/>
+
+      <section class="flex-1 relative overflow-hidden bg-grid-dots">
+        <Blueprint />
+      </section>
+
+    </main>
+
+  </div>
+
+</template>
