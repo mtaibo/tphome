b78881353060423189a0e97bb2b23edc169e54dd
Miguel Taibo <dev@migueltaibo.com>
2026-05-22 22:27:20 +0200

    feat: add SettingsBottomBar for mobile navigation

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,6 +3,7 @@
     import { ref } from 'vue'
 
     import Sidebar from '@/layout/Sidebar.vue'
+    import SettingsBottomBar from '@/layout/settings/SettingsBottomBar.vue'
     import ActiveDevices from '@/layout/settings/ActiveDevices.vue'
     import PendingDevices from '@/layout/settings/PendingDevices.vue'
     import JsonEditor from '@/layout/settings/JsonEditor.vue'
@@ -17,11 +18,13 @@
 
         <Sidebar v-model:activeItem="activeItem" />
 
-        <main class="flex-1 overflow-hidden">
+        <main class="flex-1 overflow-hidden pb-18 md:pb-0">
             <ActiveDevices v-if="activeItem === 'active'" />
             <PendingDevices v-else-if="activeItem === 'pending'" />
             <JsonEditor v-else-if="activeItem === 'json'" />
         </main>
+
+        <SettingsBottomBar v-model:activeItem="activeItem" />
 
     </div>
 
