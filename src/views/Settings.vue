90f6e6ca96dd69e524bf817d37808adede959802
dev@migueltaibo.com <dev@migueltaibo.com>
2026-06-25 09:02:10 +0200

    fix: pass sections prop to Bottombar in Settings view

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -24,7 +24,7 @@
             <component :is="activeComponent" />
         </main>
 
-        <Bottombar v-model:activeSection="activeSection" />
+        <Bottombar v-model:activeSection="activeSection" :sections="sections"/>
 
     </PageLayout>
 
