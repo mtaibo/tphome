1eb60fac7163e6be7e8166571f0913e47149a1d2
Miguel Taibo <dev@migueltaibo.com>
2026-07-03 22:57:30 +0200

    fix: use dynamic viewport height to handle Safari navigation bar

--- src/layout/PageLayout.vue
+++ src/layout/PageLayout.vue
@@ -1,7 +1,7 @@
 
 <template>
 
-    <div class="flex h-screen w-screen overflow-clip font-sans bg-tp-bg text-tp-text">
+    <div class="flex h-dvh w-screen overflow-clip font-sans bg-tp-bg text-tp-text">
 
         <slot></slot>
 
