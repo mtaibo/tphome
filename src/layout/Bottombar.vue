5515513b8052129fe9482c8d540abe230375eba3
Miguel Taibo <dev@migueltaibo.com>
2026-07-04 15:44:57 +0200

    fix: raise bottombar position to 1rem

--- src/layout/Bottombar.vue
+++ src/layout/Bottombar.vue
@@ -158,7 +158,7 @@
 
     .liquid-bar {
         @apply fixed left-4 right-4 z-50 rounded-[32px] px-1.5 py-1.5 flex items-center justify-between;
-        bottom: 0.75rem;
+        bottom: 1rem;
         position: fixed;
         background: linear-gradient(
             135deg,
