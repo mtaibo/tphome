3f3451815ba9c48d91f9e676461cba004b6d3f03
Miguel Taibo <dev@migueltaibo.com>
2026-07-04 22:15:33 +0200

    fix: change status-bar-style to default and revert safe-area padding

--- src/layout/PageLayout.vue
+++ src/layout/PageLayout.vue
@@ -1,7 +1,7 @@
 
 <template>
 
-    <div class="flex h-full w-screen overflow-hidden font-sans bg-tp-bg text-tp-text" style="padding-top: env(safe-area-inset-top)">
+    <div class="flex h-full w-screen overflow-hidden font-sans bg-tp-bg text-tp-text">
 
         <slot></slot>
 
